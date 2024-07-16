28eb7ad0ad81d92651cade687bf593e9558a302c
philanthropist-hash <techfreaksmembership@gmail.com>
2024-07-17 01:08:21 +0530

    ready to publish

---
+++ wordpress/wp-content/uploads/simply-static/temp-files/simply-static-1-1721158551/wp-admin/admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://philanthropist-hash.github.io/philanthropist/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fphilanthropist%2Fwordpress%2Fwp-admin%2Fadmin.php%3Fsimply_static_page%3D11433&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://philanthropist-hash.github.io/philanthropist/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fphilanthropist%2Fwordpress%2Fwp-admin%2Fadmin.php%3Fsimply_static_page%3D11433&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://philanthropist-hash.github.io/philanthropist/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fphilanthropist%2Fwordpress%2Fwp-admin%2Fadmin.php%3Fsimply_static_page%3D11433&reauth=1">https://philanthropist-hash.github.io/philanthropist/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fphilanthropist%2Fwordpress%2Fwp-admin%2Fadmin.php%3Fsimply_static_page%3D11433&reauth=1</a></p>
+	</body>
+</html>
